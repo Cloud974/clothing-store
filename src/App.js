b423b8e0d4f42b8ed0cdc3b0f60905687554fdaf
Michael  Fournier <michaelfournier@Michaels-MBP.fios-router.home>
2023-09-26 17:18:19 -0400

    Split category menu and category items into components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
-import './App.css';
+import CategoryMenu from './components/category-menu/category-menu.component';
 
 function App() {
 
-  const categories = [
-    {
-      id: 1,
-      title: 'Mens',
-    },
-    {
-      id: 2,
-      title: 'Womens',
-    },
-    {
-      id: 3,
-      title: 'Accessories',
-    },
-    {
-      id: 4,
-      title: 'New Arrivals',
-    },
-    {
-      id: 5,
-      title: "Last Chance",
-    },
-  ]
-
   return (
-    <div className="App">
-      {categories.map((category) => (
-        <div className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container' key={category.id}>
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <CategoryMenu />
   );
 }
 
---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,42 @@
+import CategoryItem from '../category-item/category-item.component';
+import './category-menu.styles.scss';
+
+const CategoryMenu = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Mens',
+      imageUrl: 'https://teddyfresh.com/cdn/shop/collections/Men_-_Tops_1600x.jpg?v=1694105624',
+    },
+    {
+      id: 2,
+      title: 'Womens',
+      imageUrl: 'https://teddyfresh.com/cdn/shop/collections/Women_Shop_All_1600x.jpg?v=1694105625',
+    },
+    {
+      id: 3,
+      title: 'Accessories',
+      imageUrl: 'https://teddyfresh.com/cdn/shop/collections/image_a16e8056-d025-4e36-9065-5980fcd52539_1600x.jpg?v=1694105626',
+    },
+    {
+      id: 4,
+      title: 'New Arrivals',
+      imageUrl: 'https://teddyfresh.com/cdn/shop/collections/velvetjacket_1600x.jpg?v=1694105621',
+    },
+    {
+      id: 5,
+      title: "Last Chance",
+      imageUrl: 'https://teddyfresh.com/cdn/shop/collections/collection_size_sale_banner_1600x.jpg?v=1694710253',
+    },
+  ]
+
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
