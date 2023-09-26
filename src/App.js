639a6c7c6698d894fc9a5a6f05980398a8c5bdaa
Michael  Fournier <michaelfournier@Michaels-MBP.fios-router.home>
2023-09-26 15:52:28 -0400

    Add basic categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
 function App() {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Mens',
+    },
+    {
+      id: 2,
+      title: 'Womens',
+    },
+    {
+      id: 3,
+      title: 'Accessories',
+    },
+    {
+      id: 4,
+      title: 'New Arrivals',
+    },
+    {
+      id: 5,
+      title: "Last Chance",
+    },
+  ]
+
   return (
     <div className="App">
-      Clothing Store
+      {categories.map((category) => (
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container' key={category.id}>
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
