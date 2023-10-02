de5ed2c5ece95397db259886e74fcca1cf0b69fe
Michael  Fournier <michaelfournier@Michaels-MBP.fios-router.home>
2023-10-02 13:11:12 -0400

    Add shop to routes. Add product context. Add product card component. Add temp json for placeholder products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );
